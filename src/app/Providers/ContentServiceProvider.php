0f25b2f52d5d8709fa21a1f1504e4c6e73c4ab8c
manuel <code@leunam.art>
2022-05-13 15:56:40 +0200

    [refactor] extended new default serviceprovider

--- src/app/Providers/ContentServiceProvider.php
+++ src/app/Providers/ContentServiceProvider.php
@@ -2,23 +2,14 @@
 
 namespace Laravia\Content\App\Providers;
 
-use Illuminate\Support\Facades\App;
-use Laravia\Core\App\Laravia;
-use Laravia\Core\App\Providers\LaraviaServiceProvider;
+use Laravia\Core\App\Providers\ServiceProvider;
 
-class ContentServiceProvider extends LaraviaServiceProvider
+class ContentServiceProvider extends ServiceProvider
 {
-    protected $name = 'content';
+    protected $name = "content";
 
     public function boot()
     {
-        $this->loadViewsFrom(Laravia::path()->get($this->name) . '/resources/views', $this->getPackagePrefix());
-        $this->loadMigrationsFrom(Laravia::path()->get($this->name) . '/database/migrations');
-        $this->loadSeedsFrom(Laravia::path()->get($this->name) . '/database/seeders', 'Laravia\\Content\\Database\\Seeders\\');
-
-        App::booted(function () {
-            $path = Laravia::path()->get($this->name) . '/routes/web.php';
-            $this->loadRoutesFrom($path);
-        });
+        $this->defaultBootMethod();
     }
 }
