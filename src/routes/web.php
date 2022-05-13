243ccf5d71dce23881ff1cfdeb2a7667377122e8
manuel <code@leunam.art>
2022-05-13 19:05:36 +0200

    [refactor] removed plural form from store and update route

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,5 +5,5 @@ use Laravia\Content\App\Http\Controllers\ContentController;
 
 Route::get('/laravia/contents', [ContentController::class, 'index'])->name('laravia.content::index')->middleware(['web', 'auth']);
 Route::get('/laravia/content/{id?}', [ContentController::class, 'edit'])->name('laravia.content::edit')->middleware(['web', 'auth']);
-Route::post('/laravia/contents/store', [ContentController::class, 'store'])->name('laravia.content::store')->middleware(['web', 'auth']);
-Route::post('/laravia/contents/update', [ContentController::class, 'update'])->name('laravia.content::update')->middleware(['web', 'auth']);
+Route::post('/laravia/content/store', [ContentController::class, 'store'])->name('laravia.content::store')->middleware(['web', 'auth']);
+Route::post('/laravia/content/update', [ContentController::class, 'update'])->name('laravia.content::update')->middleware(['web', 'auth']);
